00179057c47e498e8b43a1cb404b9528bd2b60da
Martin Simard <Martin Simard>
2016-12-04 12:26:35 -0500

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -55,7 +55,16 @@ console.log('Starting redux example');
 
 // 1- Reducer must have a default satate
 // 2- Te reducer function must always return a state
-var reducer = (state = {name : 'Anonymous'}, action) => {
+var stateDefault = {
+  name : 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId =1;
+var nextMovieId =1;
+
+var reducer = (state = stateDefault, action) => {
   //state = state || {name : 'Anonymous'};
 
   switch (action.type) {
@@ -64,6 +73,39 @@ var reducer = (state = {name : 'Anonymous'}, action) => {
           ...state,
           name: action.name
         };
+    case 'ADD_HOBBY':
+        return {
+          ...state,
+          hobbies: [
+            ...state.hobbies,
+            {
+              id: nextHobbyId++,
+              hobby: action.hobby
+            }
+          ]
+        };
+      case 'REMOVE_HOBBY':
+          return {
+            ...state,
+            hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+          };
+      case 'ADD_MOVIE':
+          return {
+            ...state,
+            movies: [
+              ...state.movies,
+              {
+                id: nextMovieId++,
+                title: action.title,
+                genre: action.genre,
+              }
+            ]
+          };
+      case 'REMOVE_MOVIE':
+          return {
+            ...state,
+            movies: state.hobbies.filter((movie) => movie.id !== action.id)
+          };
     default:
       return state;
   }
@@ -79,12 +121,46 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 //unsubscribe();
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Martin'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Spider-man',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Mad Max',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
 });
 
 store.dispatch({
